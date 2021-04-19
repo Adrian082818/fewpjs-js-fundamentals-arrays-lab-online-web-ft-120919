997b0e2a5969aa0a5de08f70cf647aec56e8bd4d
Adrian Mohabir <mohabiradrian@gmail.com>
2021-04-19 21:11:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const append = ["Milo", "Otis", "Garfield"];
-append.push("Odie");
+
 
 const prepend = ["Milo", "Otis", "Garfield"];
-prepend.unshift("Odie");
+
 
 const removeLast = ["Milo", "Otis", "Garfield"];
-removeLast.pop();
+
 
 const removeFirst = ["Milo", "Otis", "Garfield"];
-removeFirst.shift();
